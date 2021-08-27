d54bc11adf7882b8ff6df4be4958b64090e45e2a
Peter Bindels <dascandy@gmail.com>
2021-08-27 15:18:28 +0200

    Set msi on hpet to on (removes qemu hack); temp disable disk

--- qemu.sh
+++ qemu.sh
@@ -5,6 +5,7 @@
 objcopy -I elf64-x86-64 -O elf32-i386 build/amd64/bin/kernel build/amd64/bin/kernel.i386
 #qemu-system-x86_64 -machine q35 --cpu Skylake-Client -enable-kvm \
 qemu-system-x86_64 -machine q35 --cpu host,+x2apic -enable-kvm \
+    -global hpet.msi=true \
     -smp cores=8,sockets=1 \
     -vga none \
     -m 4G \
@@ -12,13 +13,13 @@ qemu-system-x86_64 -machine q35 --cpu host,+x2apic -enable-kvm \
     -device pcie-pci-bridge,id=pci,bus=pcie.0 \
     -device intel-hda,bus=pci,addr=1 -device hda-duplex \
     -device qemu-xhci,bus=pcie.0 -device usb-kbd -device usb-mouse \
-    -drive file=disk.img,if=none,id=D22,driver=file \
-    -device nvme,drive=D22,serial=1234 \
     -d trace:*nvme* \
     -debugcon stdio \
     -kernel build/amd64/bin/kernel.i386
 # -nic tap,ipv6=on,ipv4=off,model=e1000,mac=52:54:98:76:54:32
 
+#    -device nvme,drive=D22,serial=1234 \
+#    -drive file=disk.img,if=none,id=D22,driver=file \
 #    -d trace:usb_xhci_* \
 #    -device sdhci-pci --device sd-card \
 #    -device virtio-gpu,bus=pcie.0 \
